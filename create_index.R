7cb02d8383e92c766968935f148596f39394d322
stibu81 <slanz1137@gmail.com>
2024-10-27 18:27:29 +0100

    create webpage with images

--- create_index.R
+++ create_index.R
@@ -5,7 +5,8 @@ library(simpleTaxonomy)
 
 taxonomy <- read_taxonomy("taxonomy.csv")
 widget <- plot_taxonomy(taxonomy,
-                        expand_rank = c("Gattung", "Art"))
+                        expand_rank = c("Gattung", "Art"),
+                        show_images = TRUE)
 
 htmlwidgets::saveWidget(widget, "index.html",
                         title = "taxonomyData")
