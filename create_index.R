576696e477a27c9d7d20156619c7acf7a2706dec
stibu81 <slanz1137@gmail.com>
2025-12-09 22:36:56 +0100

    update script for creation of html file

--- create_index.R
+++ create_index.R
@@ -2,11 +2,13 @@
 # https://stibu81.github.io/taxonomyData/
 
 library(simpleTaxonomy)
+library(magrittr)
+library(htmlwidgets)
+library(htmltools)
 
 taxonomy <- read_taxonomy("taxonomy.csv")
 widget <- plot_taxonomy(taxonomy,
                         expand_rank = c("Gattung", "Art"),
+                        image_size = 250,
                         show_images = TRUE)
-
-htmlwidgets::saveWidget(widget, "index.html",
-                        title = "taxonomyData")
+save_taxonomy(widget, "index.html", title = "taxonomyData")
