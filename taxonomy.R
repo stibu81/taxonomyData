1d63d90e8d556cfa88e13c91367e0de184b769fc
stibu81 <slanz1137@gmail.com>
2026-05-03 11:53:33 +0200

    pass contact data to wikipedia api

--- taxonomy.R
+++ taxonomy.R
@@ -3,7 +3,15 @@
 
 library(simpleTaxonomy)
 
-taxonomy <- enrich_taxonomy_with_images("taxonomy.csv")
+# store your Wikipedia contact data in a file called "contact".
+# It should contain two lines with language code and username, e.g.
+# de
+# Me
+contact <- if (file.exists("contact")) {
+  setNames(as.list(readLines("contact")), c("lang", "user"))
+}
+taxonomy <- enrich_taxonomy_with_images("taxonomy.csv", contact = contact)
+
 
 taxonomy <- read_taxonomy("taxonomy.csv")
 plot_taxonomy(taxonomy,
@@ -13,7 +21,7 @@ plot_taxonomy(taxonomy,
               highlight = c(),
               expand_rank = c("Gattung", "Art"),
               show_images = TRUE,
-              image_size = "200"
+              image_size = "250"
               )
 
 
